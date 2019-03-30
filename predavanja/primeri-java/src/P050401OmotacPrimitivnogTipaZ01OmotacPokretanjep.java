b5d549adc176ef2b93fffbabb50f84fdd3c8d6d3
Vladimir Filipovic <vladofilipovic@hotmail.com>
2019-03-30 15:25:19 +0100

    popravio

---
+++ predavanja/primeri-java/src/P050401OmotacPrimitivnogTipaZ01OmotacPokretanjep.java
@@ -0,0 +1,16 @@
+class P050401OmotacPrimitivnogTipaZ01OmotacPokretanjep {
+
+    public static void main(String[] args) {
+        //tzv. "omotač" referentni tipovi za primitive tipove
+        Integer ceoBroj1 = new Integer(3);
+        Integer ceoBroj2 = 3; //skraćeni zapis
+        Short ceoBroj3 = 10;
+        Float realniBroj1 = 4.4f;
+        Double realniBroj2 = 3.3;
+        /* "omotač" tip za neki primitivni tip je referentni tip
+         * u koji je umetnuta promenljiva odgovarajućeg primitivnog tipa
+         */
+    }
+}
+
+
