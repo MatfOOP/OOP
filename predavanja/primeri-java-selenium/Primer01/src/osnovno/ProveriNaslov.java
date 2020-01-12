ee78fc6780223b6b1311ab9eb3217975c02eb9b3
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-01-12 08:43:49 +0100

    poporavio

---
+++ predavanja/primeri-java-selenium/Primer01/src/osnovno/ProveriNaslov.java
@@ -0,0 +1,37 @@
+package osnovno;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class ProveriNaslov {
+
+
+    public static void main(String[] args) {
+        // declaration and instantiation of objects/variables
+		System.setProperty("webdriver.chrome.driver","D:\\Appl\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+       
+    }
+
+}
