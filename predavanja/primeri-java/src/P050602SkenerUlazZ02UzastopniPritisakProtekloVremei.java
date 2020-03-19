32024b232c671f24dfb6f9d60756457a1abc49be
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-03-19 22:36:00 +0100

    dodao

--- predavanja/primeri-java/src/P050602SkenerUlazZ02UzastopniPritisakProtekloVremei.java
+++ predavanja/primeri-java/src/P050602SkenerUlazZ02UzastopniPritisakProtekloVremei.java
@@ -5,24 +5,17 @@
  
  */
 
-// Задатак представља илустрацију мерења трајања неког временског
-// интервала
-
-package rs.math.oop1.p050602.merenjeVremena.z01.protekloVreme;
-
-public class VremeIzvrsenjaMetoda {
+public class P050602SkenerUlazZ02UzastopniPritisakProtekloVremei {
 
    public static void main(String[] args) {
       java.util.Scanner skener = new java.util.Scanner(System.in);
-      System.out.println(
-            "Pritisni dva puta taster <Enter>!");
+      System.out.println("Pritisni dva puta taster <Enter> !!!");
       skener.nextLine();
       long vremePocetka = System.nanoTime();
+      System.out.println("Jos jednom pritisni taster <Enter> !!!");
       skener.nextLine();
       double trajanje = (System.nanoTime() - vremePocetka) / 1e9;
-      System.out.printf(
-            "Izmedju dva pritiska je proteklo %f sekundi!",
-            trajanje);
+      System.out.printf("Izmedju dva pritiska je proteklo %f sekundi!", trajanje);
       skener.close();
    }
 
