9507e70784814e9613e7e9c8c9992d83f737180d
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-03-31 11:21:44 +0200

    поправио

---
+++ predavanja/primeri-java/src/rs/math/oop1/z080402/solid/z01/losS/Korisnik.java
@@ -0,0 +1,6 @@
+package rs.math.oop1.z080402.solid.z01.losS;
+
+public class Korisnik {
+   String ime;
+   String prezime;
+}
---
+++ predavanja/primeri-java/src/rs/math/oop1/z080402/solid/z02/dobarS/Korisnik.java
@@ -0,0 +1,6 @@
+package rs.math.oop1.z080402.solid.z02.dobarS;
+
+public class Korisnik {
+   String ime;
+   String prezime;
+}
