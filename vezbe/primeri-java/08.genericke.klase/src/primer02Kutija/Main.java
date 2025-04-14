38fb9d48aace75c5fbcc886f2d55f899297c608e
miloje98 <joksimovic21@gmail.com>
2025-04-14 13:31:11 +0200

    Resolve komentara

--- vezbe/primeri-java/08.genericke.klase/src/primer02Kutija/Main.java
+++ vezbe/primeri-java/08.genericke.klase/src/primer02Kutija/Main.java
@@ -5,13 +5,17 @@ public class Main
     public static void main(String[] args)
     {
         // Tip predmeta nase kutije ce biti Integer
-        Kutija<Integer> kutija = new Kutija<>();
+        // Razlog zasto nam je potreban Integer a ne int je sto se zahteva klasni a ne primitivni tip
+        Kutija<Integer> kutija = new Kutija<>(); // Identicno kao Kutija<Integer> kutija = new Kutija<Integer>(), kompajler moze to sam da zakljuci
         System.out.println("Ubacuje se predmet 10");
         kutija.ubaciPredmet(10);
         System.out.println(kutija);
 
         System.out.println("Uklanja se predmet");
-        kutija.uzmiPredmet();
+        // Primetimo da iako uzmiPredmet vraca Integer,
+        // kompajler omogucava automatsku konverziju u primitivni tip
+        int izvadjenPredmet = kutija.uzmiPredmet();
+        System.out.println("Izvadjen predmet: " + izvadjenPredmet);
         System.out.println(kutija);
     }
 }
--- vezbe/primeri-java/08.genericke.klase/src/primer04Par/UredjeniPar.java
+++ vezbe/primeri-java/08.genericke.klase/src/primer04Par/UredjeniPar.java
@@ -2,7 +2,8 @@ package primer04Par;
 
 // U ovom slucaju radimo s klasom UredjeniPar.
 // Kako u uredjenom paru mozemo da imamo par razlicith tipova
-// tako ce i nasa genericka klasa sada imati dva parametra T1.
+// tako ce i nasa genericka klasa sada imati dva parametra T1 i T2
+// T1 i T2 mogu, ali ne moraju biti istog tipa
 public class UredjeniPar<T1, T2>
 {
     private T1 a;
